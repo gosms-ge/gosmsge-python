2b060ee300f203fa8e62377883fdf5e784f1c2c9
Luka Tavkhelidze <52665503+tavkhelidzeluka@users.noreply.github.com>
2021-07-25 04:31:44 +0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+with open("README.MD", "r", encoding="utf-8") as readme_file:
+    long_description = readme_file.read()
+
+setup(
+    name='gosmsge',
+    version='1.0.0',
+    description='Package for GoSMSGE that helps you to send messages to your clients',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Graey',
+    project_urls={
+        'Source': 'https://github.com/tavkhelidzeluka/gosmsge.git',
+    },
+    author_email='info@graey.ge',
+    packages=find_packages(include=[
+        'gosmsge'
+    ]),
+    install_requires=[
+        'requests>=2.25.1'
+    ],
+    extras_require={
+        'dev': [
+            'requests-mock>=1.9.3'
+        ]
+    },
+    license='MIT',
+    license_file='LICENSE',
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    test_suite='tests',
+)
