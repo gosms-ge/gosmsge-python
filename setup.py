6ced02302c1c8cc50d4c838c7b5429907260551f
Luka Tavkhelidze <52665503+tavkhelidzeluka@users.noreply.github.com>
2021-08-06 17:46:55 +0400

    Fix bug couldn't import gosms

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.MD", "r", encoding="utf-8") as readme_file:
 
 setup(
     name='gosmsge',
-    version='1.0.3',
+    version='1.0.4',
     description='Package for GoSMSGE that helps you to send messages to your clients',
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -15,7 +15,7 @@ setup(
     },
     author_email='info@graey.ge',
     packages=find_packages(include=[
-        'gosmsge'
+        'gosms'
     ]),
     install_requires=[
         'requests>=2.25.1',
